4d2e9dbc9b035683f36cb9efa2ed108a6cdf6285
jinyang.yjy <yujinyang2010@163.com>
2016-03-22 22:50:44 +0800

    实现简单功能

--- app/src/main/java/yjy/com/accounts/function/note/AccountActivity.java
+++ app/src/main/java/yjy/com/accounts/function/note/AccountActivity.java
@@ -1,6 +1,5 @@
-package yjy.com.accounts;
+package yjy.com.accounts.function.note;
 
-import android.accounts.Account;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,13 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
+import yjy.com.accounts.R;
 import yjy.com.accounts.application.ACConst;
 import yjy.com.accounts.databases.AccountInfo;
 import yjy.com.accounts.databases.helper.ACDBHelper;
@@ -47,10 +49,10 @@ public class AccountActivity extends AppCompatActivity {
             public void onClick(View v) {
                 List<AccountInfo> accounts = ACDBHelper.getInstance(AccountActivity.this).loadAllAccount();
                 StringBuilder sb = new StringBuilder();
-                for(AccountInfo accountInfo : accounts){
-                    sb.append(convert2String(accountInfo)+"\n");
+                for (AccountInfo accountInfo : accounts) {
+                    sb.append(convert2String(accountInfo) + "\n");
                 }
-                ((TextView)findViewById(R.id.text)).setText(sb.toString());
+                ((TextView) findViewById(R.id.text)).setText(sb.toString());
             }
         });
 
@@ -58,6 +60,26 @@ public class AccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 addAccount();
+            }
+        });
+
+        Spinner mySpinner = (Spinner)findViewById(R.id.use_spanner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.usages));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //第四步：将适配器添加到下拉列表上
+        mySpinner.setAdapter(adapter);
+        //第五步：为下拉列表设置各种事件的响应，这个事响应菜单被选中
+        mySpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                /* 将所选mySpinner 的值带入myTextView 中*/
+//                myTextView.setText("您选择的是："+ adapter.getItem(arg2));
+                /* 将mySpinner 显示*/
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+//                myTextView.setText("NONE");
             }
         });
     }
